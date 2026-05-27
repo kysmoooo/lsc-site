55d44b5b44d850faf6524e211092c47135ef554a
kysmoooo <kylian090@gmail.com>
2026-05-27 14:16:00 +0000

    ajout du switch - correction #4

--- app.py
+++ app.py
@@ -159,7 +159,6 @@ def employe_page():
 
 @app.route("/direction.html")
 def direction_page():
-    session.pop('direction_id', None)
     return render_template('direction.html')
 
 @app.route("/commande.html")
